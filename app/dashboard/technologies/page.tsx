2993b9320a5e73388214cfe1615a588d0d64a365
Florian <florian.hempelmann@gmx.de>
2026-09-09 20:24:27 +0200

    feature/setup-dashboard: added dashboard layout, page meta information, sidenav

---
+++ app/dashboard/technologies/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from 'next';
+export const metadata : Metadata = {
+    title: 'Technologies and Tools',
+    description: 'A list of all technologies and tools I used with some detailed info.',
+}
+
+export default function technologies() {
+    return (
+        <div>test</div>
+    )
+}
