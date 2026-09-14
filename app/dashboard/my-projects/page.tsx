dfb65efc5af72aa991afd6322c1d2792f5ea75ee
Florian Hempelmann <florian.hempelmann@gmx.de>
2026-09-14 21:44:37 +0200

    feature/setup-dashboard (#1)
    * feature/setup-dashboard: added dashboard layout, page meta information, sidenav
    
    * feature/setup-dashboard: edited README
    
    * feature/setup-dashboard: delete default md files I don't use

---
+++ app/dashboard/my-projects/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from 'next';
+export const metadata : Metadata = {
+    title: 'My Projects',
+    description: 'A list of Projects I plan or do with come extra information.',
+}
+
+export default function myProjects() {
+    return (
+        <div>test</div>
+    )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from 'next';
+export const metadata : Metadata = {
+    title: 'Home',
+    description: 'Overview of my learnings with visual elements.',
+}
+
+export default function Dashboard() {
+    return (
+        <div>test</div>
+    )
+}
