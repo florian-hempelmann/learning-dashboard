23119c383bf208f5795ddfbdb6b786c59d40537e
Florian <florian.hempelmann@gmx.de>
2026-09-17 01:04:57 +0200

    feature/setup-database: implementing first general frontend themes and elements based on Mockups

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import SideNav from '@/app/ui/dashboard/sidenav';
+import SideNav from '@/app/components/dashboard/sidenav';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -12,11 +12,11 @@ export const metadata: Metadata = {
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        <main className="flex flex-col md:flex-row md:overflow-hidden">
             <div className="w-full flex-none md:w-64">
                 <SideNav />
             </div>
             <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
-        </div>
+        </main>
     );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
+import {Header} from "@/app/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +27,17 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="flex min-h-screen flex-col bg-background text-foreground">
+        <Providers>
+          <Header />
+            <main className="flex-1">
+              {children}
+            </main>
+        </Providers>
+      </body>
     </html>
   );
 }
